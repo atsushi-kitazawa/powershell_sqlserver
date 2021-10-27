572c4b2132b8782939e4b0e3bedd2db5a5fdaf5e
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-10-28 00:55:36 +0900

    split var ipaddress, port number.

--- sqlserver.ps1
+++ sqlserver.ps1
@@ -1,7 +1,7 @@
 ####################
 # constant variable
 ####################
-$server = "127.0.0.1,1433"
+$server = "127.0.0.1"
 $port = 1433
 $database = "testdb"
 $user = "sa"
@@ -32,8 +32,30 @@ function select_t1([string]$pServer, [string]$pDatabase, [string]$pUser, [string
     $pSQLConnection.Close()
 }
 
+function insert_varbinary_from_file([string]$pServer, [string]$pDatabase, [string]$pUser, [string]$pPassword) {
+    # Connection
+    $pSQLConnection = New-Object System.Data.SqlClient.SqlConnection
+    $pSQLConnection.ConnectionString = "Data Source=$($pServer);Initial Catalog=$($pDatabase);User ID=$($pUser);Password=$($pPassword)"
+    $pSQLConnection.Open()
+
+    # Query
+    $query = "select file_contents from t2"
+
+    # Command
+    [System.Data.SqlClient.SqlCommand]$cmd = New-Object System.Data.SqlClient.SqlCommand($query, $pSQLConnection)
+
+    # Execute and Get scalar value
+    [byte[]]$return = $cmd.ExecuteScalar()
+    
+    # Close Connection
+    $pSQLConnection.Close()
+
+    # Result to pipe
+    return $return
+}
+
 function main() {
-    select_t1 -pServer $server -pDatabase $database -pUser $user -pPassword $password
+    select_t1 -pServer "$server,$port" -pDatabase $database -pUser $user -pPassword $password
 }
 
 main
