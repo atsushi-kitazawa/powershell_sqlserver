4fc22dbba5d61d1669196ccb2bda7f6aef31a91c
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-10-29 00:41:47 +0900

    implement openConnection, closeConnection functions

--- sqlserver.ps1
+++ sqlserver.ps1
@@ -7,12 +7,7 @@ $database = "testdb"
 $user = "sa"
 $password = "P@ssw0rd"
 
-function select_t1([string]$pServer, [string]$pDatabase, [string]$pUser, [string]$pPassword) {
-    # Connection
-    $pSQLConnection = New-Object System.Data.SqlClient.SqlConnection
-    $pSQLConnection.ConnectionString = "Data Source=$($pServer);Initial Catalog=$($pDatabase);User ID=$($pUser);Password=$($pPassword)"
-    $pSQLConnection.Open()
-    
+function select_t1([System.Data.SqlClient.SqlConnection]$pSQLConnection) {    
     # Query
     $query = "select id, name from t1"
 
@@ -27,17 +22,9 @@ function select_t1([string]$pServer, [string]$pDatabase, [string]$pUser, [string
         Write-Output $rows['name']
     }
     $rows.Close()
-
-    # Close Connection
-    $pSQLConnection.Close()
 }
 
-function select_varbinary([string]$pServer, [string]$pDatabase, [string]$pUser, [string]$pPassword) {
-    # Connection
-    $pSQLConnection = New-Object System.Data.SqlClient.SqlConnection
-    $pSQLConnection.ConnectionString = "Data Source=$($pServer);Initial Catalog=$($pDatabase);User ID=$($pUser);Password=$($pPassword)"
-    $pSQLConnection.Open()
-
+function select_varbinary([System.Data.SqlClient.SqlConnection]$pSQLConnection) { 
     # Query
     $query = "select file_contents from t2"
 
@@ -46,41 +33,46 @@ function select_varbinary([string]$pServer, [string]$pDatabase, [string]$pUser, 
 
     # Execute and Get scalar value
     [byte[]]$return = $cmd.ExecuteScalar()
-    
-    # Close Connection
-    $pSQLConnection.Close()
 
     # Result to pipe
     return $return
 }
 
-function insert_file_to_varbinary([string]$pServer, [string]$pDatabase, [string]$pUser, [string]$pPassword, [string]$pFilename, [string]$pFilepath) {
-        # Connection
-        $pSQLConnection = New-Object System.Data.SqlClient.SqlConnection
-        $pSQLConnection.ConnectionString = "Data Source=$($pServer);Initial Catalog=$($pDatabase);User ID=$($pUser);Password=$($pPassword)"
-        $pSQLConnection.Open()
+function insert_file_to_varbinary([System.Data.SqlClient.SqlConnection]$pSQLConnection, [string]$pFilename, [string]$pFilepath) {    
+    # Query
+    $query = "insert into t2(file_id, file_contents) select '{0}' as file_id, * from OPENROWSET(BULK N'{1}', SINGLE_BLOB) AS file_contents"
+    $query = [String]::Format($query, $pFilename, $pFilepath)
     
-        # Query
-        $query = "insert into t2(file_id, file_contents) select '{0}' as file_id, * from OPENROWSET(BULK N'{1}', SINGLE_BLOB) AS file_contents"
-        $query = [String]::Format($query, $pFilename, $pFilepath)
+    # Command
+    [System.Data.SqlClient.SqlCommand]$cmd = New-Object System.Data.SqlClient.SqlCommand($query, $pSQLConnection)
     
-        # Command
-        [System.Data.SqlClient.SqlCommand]$cmd = New-Object System.Data.SqlClient.SqlCommand($query, $pSQLConnection)
-    
-        # Execute and Get scalar value
-        $ret = $cmd.ExecuteNonQuery()
-        Write-Output $ret
+    # Execute and Get scalar value
+    $ret = $cmd.ExecuteNonQuery()
+    Write-Output $ret
+}
 
-        # Close Connection
-        $pSQLConnection.Close()
+function openConnection([string]$pServer, [string]$pDatabase, [string]$pUser, [string]$pPassword) {
+    $pSQLConnection = New-Object System.Data.SqlClient.SqlConnection
+    $pSQLConnection.ConnectionString = "Data Source=$($pServer);Initial Catalog=$($pDatabase);User ID=$($pUser);Password=$($pPassword)"
+    $pSQLConnection.Open()
+    return $pSQLConnection
+}
+
+function closeConnection([System.Data.SqlClient.SqlConnection]$pSQLConnection) {
+    $pSQLConnection.Clone()
 }
 
 function main() {
-    #select_t1 -pServer "$server,$port" -pDatabase $database -pUser $user -pPassword $password
+    # open connection
+    $conn = openConnection -pServer "$server,$port" -pDatabase $database -pUser $user -pPassword $password
 
-    #select_varbinary -pServer "$server,$port" -pDatabase $database -pUser $user -pPassword $password | Set-Content "sqlcmd.bin" -Encoding Byte
+    # exec querys
+    select_t1 -pSQLConnection $conn
+    select_varbinary -pSQLConnection $conn | Set-Content "sqlcmd.bin" -Encoding Byte
+    insert_file_to_varbinary -pSQLConnection $conn -pFileName "bbb.txt" -pFilepath '/var/opt/mssql/data/aaa.txt'
 
-    insert_file_to_varbinary -pServer "$server,$port" -pDatabase $database -pUser $user -pPassword $password -pFileName "aaa.txt" -pFilepath '/var/opt/mssql/data/aaa.txt'
+    # close connection
+    closeConnection -pSQLConnection $conn | Out-Null
 }
 
 main
